f48760e2d7442cb0b029f1b1b9ac32318877fcd9
shaggyishere <blues.fede@gmail.com>
2022-09-07 11:21:41 +0200

    done little changes to improve readability

--- csv-searcher.js
+++ csv-searcher.js
@@ -1,18 +1,21 @@
 const { parse } = require('csv-parse')
 const fs = require('fs')
 const arguments = process.argv.slice(2);
+const filePath = arguments[0];
+const columnIndex = arguments[1];
+const keyword = arguments[2];
 
 if(arguments.length <= 2) {
     console.log("<usage>: csv-searcer.js file_path.csv column_index keyword ")
     process.exit(1);
 }
 
-if(isNaN(arguments[1])) {
+if(isNaN(columnIndex)) {
 	console.log("First parameter must be an integer");
 	process.exit(1);
 } 
 
-fs.createReadStream(arguments[0])
+fs.createReadStream(filePath)
 	.on('error', (err) => {
 		console.log(err.message)
 	})
@@ -25,8 +28,6 @@ fs.createReadStream(arguments[0])
 		console.log(err.message)
 	})
 	.on('data', (data) => {
-		if(data[arguments[1]] === arguments[2]) {
-			const result = Object.keys(data).map((k) => data[k]);
-			console.log(result.toString());
-		}
+		if(data[columnIndex] === keyword) 
+			console.log(`${data.toString()};`);
 	});
